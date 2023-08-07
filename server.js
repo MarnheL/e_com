06d34342cc130caa8016952b665da882c57ecbc7
MarnheL <marnhel131@gmail.com>
2023-08-07 21:28:52 +0800

    change db

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
         // const conn = await mongoose.connect('mongodb://localhost:27017/ordering_system')
+
+        // const conn = await mongoose.connect('mongodb://localhost:27017/ordering_system')
         // console.log(`Successfully Connected To The Database ${conn.connection.host}`)
         console.log(`Successfully Connected To The Database`)
         expiryDate()
